a3fd02b35e0d1589bdd7fd20990dd29b129fef6c
RFilho01 <robertocorreiafilho0100@outlook.com>
2023-11-05 21:37:18 -0300

    feat: add database schemas and logs

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/RFilho01/gopportunities/router"
+import (
+	"fmt"
+
+	"github.com/RFilho01/gopportunities/config"
+	"github.com/RFilho01/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
